eac6158be767705099e86c705c193d820a6e6f76
Obiang <saintgermes1996@gmail.com>
2020-08-25 15:02:14 +0100

    build aspect distance

--- data_process.py
+++ data_process.py
@@ -13,7 +13,8 @@ def get_word_aspect_distance(aspect_start_position,aspect_end_position, word_pos
 
 #returne la liste des mots d'une phrase et la position de chaque mot
 def get_word_with_indice(sentence):
-    tokens = ['je', 'mange', 'la', 'banane']
+    # tokens = ['je', 'mange', 'la', 'banane']
+    tokens = nltk.word_tokenize(sentence)
     data = []
     i = 0
     for token in tokens:
@@ -23,18 +24,17 @@ def get_word_with_indice(sentence):
 
 #deternime les coordonnee de l'aspet dans la phrase
 def get_aspect_position(sentence, aspect):
-    tokens = ['je', 'mange', 'la', 'fausse', 'banane']
-    aspect_token = ['la', 'fausse']
-    i = 0
+    tokens = nltk.word_tokenize(sentence)
+    aspect_token = nltk.word_tokenize(aspect)
     aspect_length = len(aspect_token)
     start_positio = 0
     end_positio = 0
     for i in range(len(tokens)):
-        print(tokens[i], aspect_token[0])
+        # print(tokens[i], aspect_token[0])
         if tokens[i] == aspect_token[0]:
             match = True
             for j in range(len(aspect_token)):
-                print(tokens[i+j], aspect_token[j])
+                # print(tokens[i+j], aspect_token[j])
                 if tokens[i+j] != aspect_token[j]:
                     match = False
             if match:
@@ -46,10 +46,15 @@ def get_aspect_position(sentence, aspect):
 
 # Sentence reprensentation contain sentence, aspect, polarity, start and end
 def get_sentence_vector(sentence_representatio):
-    print('noting')
-
-
-print(get_word_aspect_distance(aspect_start_position=2, aspect_end_position=3, word_position=6))
-# print(get_aspect_position("je mange la banane", "la"))
-
-# print(["yo","ya"] == ["yo","yy"])
+    result = []
+    for sent_rep in sentence_representatio:
+        sentence = sent_rep['sentence']
+        aspect = sent_rep['aspect']
+        data = get_word_with_indice(sentence)
+        start, end = get_aspect_position(sentence, aspect)
+        oneElmt = []
+        for elm in data:
+            oneElmt.append(get_word_aspect_distance(aspect_start_position=start, aspect_end_position=end,
+                                           word_position=elm['position']))
+        result.append(oneElmt)
+    return result
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from utils import *
+from data_process import *
 base_path = ''
 raw_train_path = os.path.join(base_path, 'data/test.xml')
 test_data = parse_sentence_term_for_sentiment(raw_train_path)
-print(test_data[3])
+data = get_sentence_vector(test_data)
+print(test_data[3]['sentence'])
+print(data[3])
